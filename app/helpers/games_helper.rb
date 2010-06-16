01aae0d52acc06e21ed21c932b6fde1f3f3c86cc
Ernesto Tagwerker <ernesto.tagwerker@changeleader.com>
2010-06-16 18:38:44 -0300

    Refactored code.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  include ForecastsHelper
+  
+  # determines if someone can edit a game or not
+  # only admin users can
+  def show_edit_link?
+    @user && @user.admin
+  end
 end
