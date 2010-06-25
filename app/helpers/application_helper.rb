6042a1c745fa55f0b553a6292da46a0dde1efcb0
Ernesto Tagwerker <ernesto.tagwerker@changeleader.com>
2010-06-25 18:36:30 -0300

    Corrected helper method nam,e Corrected helper method nam,e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   # you can only edit your own forecasts
-  def show_edit_link? 
+  def show_forecasts_edit_link? 
     result = false
     if params[:user] && @user && @user.nick
       result = (params[:user] == @user.nick) && !is_first_phase_on?
--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,3 +1,3 @@
 module SiteHelper
-  
+    
 end
