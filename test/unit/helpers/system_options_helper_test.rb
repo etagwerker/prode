1f9167fd24a62391610b417dfad261e6d1dcde0c
Ernesto Tagwerker <ernesto@ombushop.com>
2014-06-04 10:19:43 -0300

    Wont test anything yet.

--- test/unit/helpers/system_options_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SystemOptionsHelperTest < ActionView::TestCase
-end
--- test/unit/top_four_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class TopFourTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
