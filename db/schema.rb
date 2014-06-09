3fa18288031483f8242462f996f158f51b27df61
Ernesto Tagwerker <ernesto@ombushop.com>
2014-06-09 00:17:40 -0300

    Added schema and code to seed db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608134912) do
+ActiveRecord::Schema.define(version: 20140609021203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "forecasts", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.integer  "home_goals"
+    t.integer  "away_goals"
+    t.integer  "winner_id"
+    t.integer  "tournament_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "games", force: true do |t|
-    t.integer  "local_id",     null: false
-    t.integer  "visitor_id",   null: false
+    t.integer  "local_id",                 null: false
+    t.integer  "visitor_id",               null: false
     t.integer  "group_id"
+    t.integer  "home_goals",   default: 0
+    t.integer  "away_goals",   default: 0
+    t.integer  "winner_id"
     t.datetime "time"
     t.integer  "order_number"
     t.string   "city"
@@ -35,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140608134912) do
 
   create_table "teams", force: true do |t|
     t.string   "name",                      null: false
+    t.string   "slug"
     t.integer  "group_id",                  null: false
     t.integer  "points",        default: 0
     t.integer  "goals_for",     default: 0
@@ -43,5 +71,32 @@ ActiveRecord::Schema.define(version: 20140608134912) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tournaments", force: true do |t|
+    t.string   "name"
+    t.integer  "owner_id"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tournaments_users", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "tournament_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "omniauth_provider"
+    t.text     "omniauth_hash"
+    t.string   "omniauth_uid"
+    t.string   "profile_image_url"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["omniauth_provider", "omniauth_uid"], name: "index_users_on_omniauth_provider_and_omniauth_uid", unique: true, using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,33 @@ require 'json'
 
 json = JSON.load(File.open(File.join(Rails.root, 'db/seeds.json')))
 
-puts json
+puts "Seeding"
+
+# Returns a Time object using the hash (See seeds.json)
+#
+# @param hash [Hash]
+# @return [Time]
+def get_time(hash)
+  Time.parse("#{hash['time']} #{hash['timezone']}")
+end
 
 json.each do |game|
   group = Group.where(name: game['group']).first ||
             Group.create(name: game['group'])
-  
+
   local = Team.where(name: game['local']).first ||
             Team.create(name: game['local'], group_id: group.id)
   visitor = Team.where(name: game['visitor']).first ||
               Team.create(name: game['visitor'], group_id: group.id)
 
   unless Game.where(local_id: local.id, visitor_id: visitor.id, group_id: group.id).first
-    Game.create(local_id: local.id, visitor_id: visitor.id, group_id: group.id)
+    Game.create(local_id: local.id,
+                visitor_id: visitor.id,
+                group_id: group.id,
+                time: get_time(game),
+                order_number: game['number'],
+                city: game['city'])
   end
 end
+
+puts "Seeded"
