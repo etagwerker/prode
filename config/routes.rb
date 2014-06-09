847dba841bb1681e334a97b40112fe68fcaff4c4
Ernesto Tagwerker <ernesto@ombushop.com>
2014-06-09 00:15:51 -0300

    Added routes for teams, tournaments, and games.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  resources :games, only: [:index]
-  resources :teams, only: [:index]
-  resources :groups, only: [:index]
+  resources :tournaments do
+    member do
+      post 'join'
+    end
+  end
+
+  resources :forecasts
+
+  resources :users
+
+  resources :games
+  resources :teams
+  resources :groups
   root "games#index"
+  get '/auth/:provider/callback' => 'sessions#create', as: :auth_callback
+  delete '/sessions/:id' => 'sessions#destroy'
+  get '/sessions/:id' => 'sessions#destroy'
 end
