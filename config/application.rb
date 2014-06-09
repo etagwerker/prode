c6d0653992ea82fe682433c6ea08be9012320202
Ernesto Tagwerker <ernesto@ombushop.com>
2014-06-09 00:17:11 -0300

    Added session store, omniauth and friendly_id init code.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Prode
   class Application < Rails::Application
+    config.time_zone = 'America/Argentina/Buenos_Aires'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_prode_session'
+Rails.application.config.session_store :cookie_store, key: ENV['COOKIE_STORE_KEY']
